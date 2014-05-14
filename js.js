55ab8965b5a58797f0721d3a6124445263f78b18
William Riley-Land <william@kun.io>
2014-05-14 17:06:10 -0500

    Format as percentage and auto populate.

--- js.js
+++ js.js
@@ -1,19 +1,21 @@
-var $email = $('input[type="email"]');
 
 // JavaScript Document
 function calculateConversionRate() {
-    var wtStr =document.form.weight.value;
-    if (!wtStr)
-        wtStr = '0';
+    var rate;
+    var wtStr = document.form.weight.value;
     var htStr = document.form.height.value;
-    if (!htStr)
-        htStr = '0';
-    var weight = parseFloat(wtStr);
-    var height = parseFloat(htStr);
-    document.form.ConversionRate.value = (weight / height).toFixed(2);
+    if (!wtStr || !htStr) {
+        rate = '--';
+    }
+    else {
+        var weight = parseFloat(wtStr);
+        var height = parseFloat(htStr);
+        var percent = (weight / height * 100).toFixed(2);
+        if (!isFinite(percent)) rate = '--';
+        else rate = percent + '%';
+    }
+    document.form.ConversionRate.value = rate;
 }
-
-
 
 function calculateViews() {
     var wtStr =document.form.ConversionRate.value;
@@ -27,8 +29,8 @@ function calculateViews() {
     document.form.weight.value = (ConversionRate * height).toFixed(2);
 }
 
-
 function valEmail(){
+    var $email = $('input[type="email"]');
 	if ($email.val().indexOf("." && "@") > -1) {
 		$('.screen').removeClass('unsuccessful').addClass('successful');
 	} else {
@@ -36,4 +38,4 @@ function valEmail(){
 	};
 };
 
-
+calculateConversionRate();
